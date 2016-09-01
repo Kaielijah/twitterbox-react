658e28122fc759fbb70d5fc23480f09d6bc4a452
Yi Ting Seah <k.seahyt@gmail.com>
2016-09-01 13:09:10 +0800

    draft version of tweetbox

---
+++ App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const CHAR_LIMIT = 160;
+
+class App extends React.Component{
+  render(){
+    return (
+
+      <div>
+      <h2>TWEETER BOX</h2>
+      <WordCount/>
+      </div>
+
+    )
+  }
+}
+
+class WordCount extends React.Component{
+  constructor(){
+    super();
+this.state={
+  chars_left : CHAR_LIMIT,
+  chars_typed: 0
+  };
+
+  }
+
+  _updateEvent(event){
+    let input = event.target.value;
+
+    if(this.state.chars_left !== 0){
+      this.setState({chars_left: CHAR_LIMIT - input.length, chars_typed: input.length
+      })
+    }else{
+      alert('TOOO LONGGS')
+    
+    }
+  }
+
+render(){
+  return(
+    <div>
+    <textarea onChange={this._updateEvent.bind(this)}></textarea>
+    <p>Characters left: {this.state.chars_left}</p>
+    <p>Characters typed: {this.state.chars_typed}</p>
+    </div>
+  )
+
+
+}
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('tweets')
+)
